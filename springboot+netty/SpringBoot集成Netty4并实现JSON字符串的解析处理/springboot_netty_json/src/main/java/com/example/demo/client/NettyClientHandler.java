ac31917aee8943e761e770f2416d30d2c0ecdff5
lzc <lzc@DESKTOP-LRS48TL>
2019-02-28 20:49:21 +0800

    1

---
+++ springboot+netty/SpringBoot集成Netty4并实现JSON字符串的解析处理/springboot_netty_json/src/main/java/com/example/demo/client/NettyClientHandler.java
@@ -0,0 +1,14 @@
+package com.example.demo.client;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+
+public class NettyClientHandler extends SimpleChannelInboundHandler<String> {
+
+	@Override
+	protected void channelRead0(ChannelHandlerContext ctx, String msg) throws Exception {
+		System.out.println("收到服务端消息: " + msg);
+	}
+
+
+}
